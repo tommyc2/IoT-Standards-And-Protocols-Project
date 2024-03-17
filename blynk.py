560826e85e1172b1b1701117d7c36c40fc5afcad
Tommy Condon <20101841@mail.wit.ie>
2024-03-17 21:17:57 +0000

    Update Blynk.py and google_maps.py with testing data

--- blynk.py
+++ blynk.py
@@ -1,29 +1,32 @@
-import BlynkLib
+import blynklib
 from time import sleep
-import requests
 import time
 import random
+import google_maps
+#from sense import SenseHat
 
-api_key = "5b2ddccbcc8054d447c55ed535cf8928"
-link = f"http://api.openweathermap.org/data/2.5/weather?units=metric&q=dungarvan,Ireland&APPID={api_key}"
-data = requests.get(link).json()
-
-#Data
-temp = data['main']['temp']
-pressure = data['main']['pressure']
+#temp = sense.get_temperature()
+#pressure = sense.get_pressure()
+#speed = google_maps.get_speed()
 
 
 BLYNK_AUTH = 'VHEbUi6uPpCsSVG5i2i4gHNQu_mrWf-H'
-# initialize Blynk
-blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
-# register handler for virtual pin V0 write event
+# initialize Blynk
+blynk = blynklib.Blynk(BLYNK_AUTH)
+
 while True:
     blynk.run()
-    blynk.virtual_write(0, temp)
-    blynk.virtual_write(1,pressure)
-    blynk.virtual_write(2,random.choice([27,24,23,20]))
-    time.sleep(.5)
-    print("Temp: ", temp)
-    print("Pressure: ", pressure)
-    print("Speed: ",random.choice([27,24,23,20]))
+    #blynk.virtual_write(0, temp)
+    #blynk.virtual_write(1,pressure)
+    temp = random.choice([27,24,23,20])
+    blynk.virtual_write(2,temp)
+    #time.sleep(.5)
+    #print("Temp: ", temp)
+    #print("Pressure: ", pressure)
+    print("Speed: ",temp)
+    #location = google_maps.get_location(52.250752, -7.137676)
+    #blynk.virtual_write(3,location) # e.g paddy brownes road
+    #time.sleep(1)
+    #print("Location: ",location)
+    
--- google_maps.py
+++ google_maps.py
@@ -1,6 +1,11 @@
 import googlemaps
+import requests
+import time
 
-gm = googlemaps.Client(key="API_KEY")
+api_key = "API_KEY"
+gm = googlemaps.Client(key=api_key)
+
+regional_road = [52.244483, -7.144931]
 
 def get_location(lat,long):
     output = gm.reverse_geocode((lat, long))
@@ -8,4 +13,13 @@ def get_location(lat,long):
     location = output[0]['formatted_address']
     return location
 
-print("Current location: ",get_location(52.172865, -7.510142))
+def get_speed():
+    return abs(25)
+
+def analyse_braking():
+    s1 = get_speed()
+    time.sleep(3)
+    s2 = get_speed()
+
+
+#print(get_location(regional_road[0],regional_road[1]))
