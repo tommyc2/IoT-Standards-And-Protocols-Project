9883edd5d36167aca87d8bcaff4c3ac666db945e
Tommy Condon <114081733+tommyc2@users.noreply.github.com>
2024-03-19 12:59:49 +0000

    Update google_maps.py

--- google_maps.py
+++ google_maps.py
@@ -2,7 +2,7 @@ import googlemaps
 import requests
 import time
 
-api_key = "API_KEY"
+api_key = "key"
 gm = googlemaps.Client(key=api_key)
 
 regional_road = [52.244483, -7.144931]
@@ -12,14 +12,6 @@ def get_location(lat,long):
 
     location = output[0]['formatted_address']
     return location
-
-def get_speed():
-    return abs(25)
-
-def analyse_braking():
-    s1 = get_speed()
-    time.sleep(3)
-    s2 = get_speed()
 
 
 #print(get_location(regional_road[0],regional_road[1]))
