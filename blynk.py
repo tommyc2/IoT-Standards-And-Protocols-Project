0b37947aac7093e2f94aab77b71b62005aab2e9d
Tommy Condon <114081733+tommyc2@users.noreply.github.com>
2024-03-19 12:57:16 +0000

    Update blynk.py

--- blynk.py
+++ blynk.py
@@ -1,32 +1,29 @@
-import blynklib
+import BlynkLib
 from time import sleep
 import time
 import random
-import google_maps
-#from sense import SenseHat
+import google_maps as gm
+from sense_hat import SenseHat
 
-#temp = sense.get_temperature()
-#pressure = sense.get_pressure()
-#speed = google_maps.get_speed()
+sense = SenseHat()
 
+temp = sense.get_temperature()
+pressure = sense.get_pressure()
+location = gm.get_location(52.244483, -7.144931)
 
-BLYNK_AUTH = 'VHEbUi6uPpCsSVG5i2i4gHNQu_mrWf-H'
+BLYNK_AUTH = 'key'
 
 # initialize Blynk
-blynk = blynklib.Blynk(BLYNK_AUTH)
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
 while True:
     blynk.run()
-    #blynk.virtual_write(0, temp)
-    #blynk.virtual_write(1,pressure)
-    temp = random.choice([27,24,23,20])
-    blynk.virtual_write(2,temp)
-    #time.sleep(.5)
-    #print("Temp: ", temp)
-    #print("Pressure: ", pressure)
-    print("Speed: ",temp)
-    #location = google_maps.get_location(52.250752, -7.137676)
-    #blynk.virtual_write(3,location) # e.g paddy brownes road
-    #time.sleep(1)
-    #print("Location: ",location)
+    blynk.virtual_write(0, temp)
+    blynk.virtual_write(1,pressure)
+    blynk.virtual_write(3,location)
+    time.sleep(5)
+    print("Temp: ", temp)
+    print("Pressure: ", pressure)
+    print("Location: ", location)
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@")
     
