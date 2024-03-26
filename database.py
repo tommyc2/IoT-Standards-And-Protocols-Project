c3d80a0f17222a4cab9fcaf6befbd42288f60dae
Tommy Condon <20101841@mail.wit.ie>
2024-03-26 13:34:56 +0000

    test

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import datetime
 from pymongo.mongo_client import MongoClient
 
-password = "pw"
+password = "dungarvan1995"
 uri = f"mongodb+srv://tcmedion:{password}@mycluster.m9wolg3.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster"
 
 client = MongoClient(uri)
@@ -38,7 +38,22 @@ data = [
     }
 ]
 
-#collection.insert_many(data)
+# 15 rows already inserted --> collection.insert_many(data)
+
+average_temp = db.collection.aggregate([
+    {
+        "$group": {
+            "_id": None,
+            "avgTemp": {"$avg": "$temp"}
+        }
+    }
+])
+
+for i in average_temp:
+    print(i["avgTemp"])
+
+
+
 
 
 
